4783ef0a49f15cee8811d17195a2a3c95fe609c7
Prakhar Kumar <prakhar.meerut@gmail.com>
2024-12-16 17:46:14 +0530

    Adding DB for catalog service
    Signed-off-by: Prakhar Kumar <prakhar.meerut@gmail.com>

--- src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -4,6 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+
+// run postgres docker locally without volume to avoid persistent storage.
+//docker run -d \
+//        --name polar-postgres \
+//        -e POSTGRES_USER=user \
+//        -e POSTGRES_PASSWORD=password \
+//        -e POSTGRES_DB=polardb_catalog \
+//        -p 5432:5432 \
+//        postgres
+
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class  CatalogServiceApplication {
